60b7403cc333da7fa20417fb1aa380f0c46a71a0
Aliaksandra Rahachova <klaustrafobiyakl@gmail.com>
2023-09-29 17:32:18 +0200

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.max = max
+        this.min = min
     }
 
     guess() {
-
+        this.currentNum = Math.ceil((this.min + this.max) / 2)
+        return this.currentNum
     }
 
     lower() {
-
+        this.max = this.currentNum
     }
 
     greater() {
-
+        this.min = this.currentNum
     }
 }
 
